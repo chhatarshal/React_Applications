54b52d6068855506a3743d262b8172b580792c42
Chhatarshal Singh <chauhan.natwal@hotmail.com>
2021-07-24 12:34:10 +0530

    Added Food summary and avialable foods components

--- FoodOrderApp/src/App.js
+++ FoodOrderApp/src/App.js
@@ -1,12 +1,17 @@
 import React, {Fragment}  from 'react';
 
 import Header from './Components/Layout/Header';
+import Meals from './Components/Meals/Meals';
+ 
 
 function App() {   
 
   return (
     <Fragment>
      <Header />
+     <main>
+        <Meals />        
+     </main>
   </Fragment>
    
   );
--- FoodOrderApp/src/Components/Layout/Header.js
+++ FoodOrderApp/src/Components/Layout/Header.js
@@ -6,7 +6,7 @@ const Header =  (props) => {
 
     return <Fragment>
         <header className={classes.header}>
-            <h1>React Meals</h1>
+            <h1>Rana Foods</h1>
            <HeaderCartButton />
         </header>
         <div className={classes['main-image']}>
