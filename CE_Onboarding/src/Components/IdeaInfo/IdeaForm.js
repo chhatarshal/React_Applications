2bab6964b8f308a5a50481150e166c1a45c26d2c
Chhatarshal Singh <chauhan.natwal@hotmail.com>
2021-07-26 22:57:34 +0530

    added header and more changes

--- CE_Onboarding/src/Components/IdeaInfo/IdeaForm.js
+++ CE_Onboarding/src/Components/IdeaInfo/IdeaForm.js
@@ -1,18 +1,21 @@
 import {Fragment} from 'react';
-import { Container, FormControl, FormLabel, Input, Center } from "@chakra-ui/react"
+import { Container, FormControl, FormLabel, Input, Center, Button, Grid, Box } from "@chakra-ui/react"
 
 
 const IdeaForm = (props) => {
 
     return <Fragment>
        <Container>
-                        <Center bg="tomato" h="100px" color="white">
-                            <FormControl id="idea" isRequired>
-                                <FormLabel size="sm">What is your Idea ?</FormLabel>
-                                <Input placeholder="Idea"  size="sm"/>
-                            </FormControl>
-                        </Center>         
-                        </Container>
+        <Center bg="light green" h="100px" color="Black">
+            <FormControl id="idea" isRequired>
+                <FormLabel size="sm">What is your Idea ?</FormLabel>
+                <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+                    <Box w="100%" h="10" > <Input placeholder="Idea"  size="lg"/> </Box>
+                    <Box w="100%" h="10" >  <Button colorScheme="blue" size="sm">Button</Button>  </Box>          
+                </Grid>
+            </FormControl>
+        </Center>         
+        </Container>
     </Fragment>
 }
 
---
+++ CE_Onboarding/src/Components/Layout/Header.js
@@ -0,0 +1,11 @@
+
+import classes from './Header.Module.css';
+
+const Header = (props) => {
+
+    return <div className={classes.mainHeader}>
+                Here is my header
+        </div>
+}
+
+export default Header;
