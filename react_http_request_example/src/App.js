93d811818cb971646b938edc5f0c714aa305401a
Chhatarshal Singh <chauhan.natwal@hotmail.com>
2021-07-27 18:05:14 +0530

    http request exaple react application

---
+++ react_http_request_example/src/App.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+
+  const [movies, setMovies] = useState([]);
+ 
+  const fetchMoviesHandler = () => {
+      fetch("https://swapi.dev/api/films/").then((response) => 
+              {return response.json()}).
+              then((data) => {
+                const transformed = data.results.map(movie => {
+                  return {
+                    id : movie.episode_id,
+                    title : movie.title,
+                    openingText : movie.opening_crawl,
+                    releaseDate : movie.release_Date
+                  }
+                })
+                setMovies(transformed);
+              }).catch(() => console.log('exception occuredd'));
+  }
+
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
