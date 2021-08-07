be9ea1284bcbd23bc33896cb282cf3c8346f1746
Chhatarshal Singh <chauhan.natwal@hotmail.com>
2021-08-07 22:57:20 +0530

    ref and usestate example

--- UserInput_Base/src/components/SimpleInput.js
+++ UserInput_Base/src/components/SimpleInput.js
@@ -1,9 +1,36 @@
+import React, {useState, useRef } from 'react';
+
+
+
+
+
 const SimpleInput = (props) => {
+
+  const [inputval, setInputval]= useState('');
+
+  const refExample = useRef('');
+
+
+  const nameInputChangeHandler = (event) => {
+      event.preventDefault();
+      console.log(event.target.value);
+      setInputval(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log('---------');
+      console.log('get ref from event ref directly,...  ');
+      console.log(refExample.current.value);
+      //setInputval('');
+  }
+  
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={refExample} type='text' id='name' value={inputval} onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
